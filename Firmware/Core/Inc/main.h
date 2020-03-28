6a7bafe350eb5ae16b8981755b7c5bbb1a9928b7
James Liu <james_liu@fastmail.com>
2020-03-28 12:10:09 +1100

    feat: add I2C config and OLED driver lib

--- Firmware/Core/Inc/main.h
+++ Firmware/Core/Inc/main.h
@@ -67,7 +67,7 @@ void Error_Handler(void);
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
-
+#define SSD1306_USE_I2C
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
